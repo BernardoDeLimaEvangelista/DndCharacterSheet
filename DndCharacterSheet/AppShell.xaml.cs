faeecb7dbe3b8d0f964d19d84d0e31fc26e23826
Bernardo <beplay4278@gmail.com>
2026-01-24 22:01:03 -0300

    Adicionar arquivos de projeto.

---
+++ DndCharacterSheet/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DndCharacterSheet
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DndCharacterSheet/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DndCharacterSheet
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
