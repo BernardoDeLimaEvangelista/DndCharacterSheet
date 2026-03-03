a72263665a60775ab0ff9d696e83912da98753d3
Bernardo <beplay4278@gmail.com>
2026-03-02 22:28:23 -0300

    Adição de algumas funcionalidades inicias do Character Builder

--- DndCharacterSheet/MainPage.xaml.cs
+++ DndCharacterSheet/MainPage.xaml.cs
@@ -6,5 +6,26 @@
         {
             InitializeComponent();
         }
+
+        private async void OnSelected(object sender, EventArgs e)
+        {
+            if (sender is Picker picker && picker.SelectedIndex != -1)
+            {
+                string? raca = null;
+                if (picker.ItemsSource is System.Collections.IList list)
+                {
+                    raca = list[picker.SelectedIndex]?.ToString();
+                }
+                else
+                {
+                    raca = picker.SelectedItem?.ToString();
+                }
+
+                if (raca != null)
+                {
+                    await DisplayAlertAsync("Ficha", $"Raça {raca} selecionada!", "OK");
+                }
+            }
+        }
     }
 }
