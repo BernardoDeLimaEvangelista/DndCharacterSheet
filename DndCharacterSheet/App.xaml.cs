7b0f8950263f882dfd5908b568efc5e7a9eb5cd3
Bernardo <beplay4278@gmail.com>
2026-03-04 14:04:55 -0300

    Ajustes técnicos (Janela em tela cheia na abertura do aplicativo)

--- DndCharacterSheet/App.xaml.cs
+++ DndCharacterSheet/App.xaml.cs
@@ -7,11 +7,46 @@ namespace DndCharacterSheet
         public App()
         {
             InitializeComponent();
+            MainPage = new AppShell();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            var window = base.CreateWindow(activationState);
+
+            // Define o título (opcional)
+            window.Title = "Character Builder";
+
+            // No Windows, precisamos de um pequeno ajuste para maximizar logo no início
+            window.Created += (s, e) =>
+            {
+#if WINDOWS
+            var nativeWindow = window.Handler.PlatformView as Microsoft.UI.Xaml.Window;
+            if (nativeWindow != null)
+            {
+                var presenter = GetAppWindowPresenter(nativeWindow);
+                presenter.Maximize();
+            }
+#endif
+
+#if MACCATALYST
+            // No Mac, o conceito é um pouco diferente, mas você pode setar o tamanho
+            window.Width = 1920; 
+            window.Height = 1080;
+#endif
+            };
+
+            return window;
         }
+
+#if WINDOWS
+    private Microsoft.UI.Windowing.OverlappedPresenter GetAppWindowPresenter(Microsoft.UI.Xaml.Window nativeWindow)
+    {
+        var windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        return appWindow.Presenter as Microsoft.UI.Windowing.OverlappedPresenter;
+    }
+#endif
     }
 }
